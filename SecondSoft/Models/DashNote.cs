1455840bd7b1e1ba68e06a73de3e3a508dba83f2
Dinand van Hartingsveldt <dinand.dotnet@gmail.com>
2022-10-15 17:38:12 +0200

    Carousel added, new labels added, replaced all about areas

---
+++ SecondSoft/Models/DashNote.cs
@@ -0,0 +1,9 @@
+﻿namespace SecondSoft.Models
+{
+    public class DashNote
+    {
+        public string Title { get; set; } = "";
+        public string Text { get; set; } = "";
+        public string Group { get; set; } = "";
+    }
+}
