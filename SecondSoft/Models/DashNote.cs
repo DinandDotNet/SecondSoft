1a028a3a68d67d94e72993f56890fc274e5b4146
Dinand van Hartingsveldt <dinand.dotnet@gmail.com>
2022-11-05 17:12:04 +0100

    css inprovements, Dashnotes inprovements Underconstruction redesign and extension

--- SecondSoft/Models/DashNote.cs
+++ SecondSoft/Models/DashNote.cs
@@ -3,7 +3,17 @@
     public class DashNote
     {
         public string Title { get; set; } = "";
-        public string Text { get; set; } = "";
+        public string Text0 { get; set; } = "";
+        public string Text1 { get; set; } = "";
+        public string Text2 { get; set; } = "";
+        public string Text3 { get; set; } = "";
+        public string Text4 { get; set; } = "";
+        public string Text5 { get; set; } = "";
+        public string Text6 { get; set; } = "";
+        public string Text7 { get; set; } = ""; 
+        public string Text8 { get; set; } = "";
+        public string Text9 { get; set; } = "";
+
         public string Group { get; set; } = "";
     }
 }
